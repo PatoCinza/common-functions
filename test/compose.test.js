a850f2d690231daf017a2dec1be76d4a7a0abafe
Gabriel.Patocinza <gabriel.luis@blueticket.com.br>
2019-12-13 14:40:54 -0300

    Reduce the number of tests to have only the necessary

--- test/compose.test.js
+++ test/compose.test.js
@@ -4,12 +4,4 @@ describe('Compose testing', () => {
   it('Should return 42', () => {
     expect(compose(x => x - 8, x => x + 10, x => x * 4)(10)).toEqual(42)
   })
-
-  it('Should compose just one function', () => {
-    expect(compose(x => true)(false)).toBe(true)
-  })
-
-  it('Should be able to not receive params', () => {
-    expect(compose(() => true, () => false)(false)).toBe(true)
-  })
 })
--- test/validate-cpf.test.js
+++ test/validate-cpf.test.js
@@ -5,14 +5,6 @@ describe('Validate cpf tests', () => {
     expect(validateCpf('723.401.510-82')).toBe(true)
   })
 
-  it('Should return false if a CPF\'s first digit is invalid', () => {
-    expect(validateCpf('723.401.510-12')).toBe(false)
-  })
-
-  it('Should return false if a CPF\'s second digit is invalid', () => {
-    expect(validateCpf('723.401.510-83')).toBe(false)
-  })
-
   it('Should test a case where calculate digit returns 0 because it\'s an untested branch of ours', () => {
     expect(validateCpf('097.565.920-01')).toBe(true)
   })
